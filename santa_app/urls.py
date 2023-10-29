711d27dbc23dac0dd6cc1694504ed72d2e955ab5
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2023-10-29 12:31:33 -0600

    Found problem
    The Item.get_url fix only directs back to the items page and not to the individual items.
    Those pages do exist when plugging in URL directly.
    Need to sort out

--- santa_app/urls.py
+++ santa_app/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#path function defines a url pattern
-#'' is empty to represent based path to app
-# views.index is the function defined in views.py
-# name='index' parameter is to dynamically create url
-# example in html <a href="{% url 'index' %}">Home</a>.
-path('', views.index, name='index'),
+    #path function defines a url pattern
+    #'' is empty to represent based path to app
+    # views.index is the function defined in views.py
+    # name='index' parameter is to dynamically create url
+    # example in html <a href="{% url 'index' %}">Home</a>.
+    path('', views.index, name='index'),
 
-path('items/', views.ItemListView.as_view(), name= 'items'),
-path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
+    path('items/', views.ItemListView.as_view(), name= 'items'),
+    path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
 
 ]
