cb22e0f75b05c0da2c5c6b935b5b38ae9bfe2694
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2023-11-18 12:07:21 -0700

    first decorator working
    decorator seeing if people are logged and trying to log in up and working

--- santa_app/decorators.py
+++ santa_app/decorators.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+def unautherized_access(view_func):
+    def wrapper_func(request, *args, **kwargs):
 
+        if request.user.is_authenticated:
+          return redirect('items')
+        else:
+            return view_func(request, *args, **kwargs)
+    
+    return wrapper_func
--- santa_app/views.py
+++ santa_app/views.py
@@ -1,9 +1,11 @@
 from typing import Any
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import *
 from django.views import generic
+
+from .models import *
 from .forms import *
+from .decorators import *
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
@@ -19,43 +21,38 @@ class ItemListView(generic.ListView):
       model = Item
 class ItemDetailView(generic.DetailView):
       model = Item
-      
 
+
+@unautherized_access
 def RegisterPage(request):
 
-     if request.user.is_authenticated:
-          return redirect('items')
-     else:
-          form = CreateUserForm()
+     form = CreateUserForm()
 
-          if request.method == 'POST':
-               form = CreateUserForm(request.POST)
-               if form.is_valid():
-                    form.save()
-                    messages.success(request, 'Account creation successful!')
-                    return redirect('login')
-
+     if request.method == 'POST':
+          form = CreateUserForm(request.POST)
+          if form.is_valid():
+               form.save()
+               messages.success(request, 'Account creation successful!')
+               return redirect('login')
 
      context = {'form':form}
      return render(request, 'santa_app/register.html', context)
 
 
+@unautherized_access
 def LoginPage(request):
 
-     if request.user.is_authenticated:
-          return redirect('items')
-     else:
-          if request.method == 'POST':
-               username = request.POST.get('username')
-               password = request.POST.get('password')
+     if request.method == 'POST':
+          username = request.POST.get('username')
+          password = request.POST.get('password')
 
-               user = authenticate(request, username=username, password=password)
+          user = authenticate(request, username=username, password=password)
 
-               if user is not None:
-                    login(request, user)
-                    return redirect('items')
-               else:
-                    messages.info(request, 'Username OR Password is incorrect')
+          if user is not None:
+               login(request, user)
+               return redirect('items')
+          else:
+               messages.info(request, 'Username OR Password is incorrect')
 
      context = {}
      return render(request, 'santa_app/login.html', context)
